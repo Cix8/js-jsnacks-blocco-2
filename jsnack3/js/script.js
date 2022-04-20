6a064be9fc45dd7bf14b95925cdbc74ae33592ab
Cix8 <francesco.natal_fn@libero.it>
2022-04-20 14:07:48 +0200

    add program in jsnack3, add type-check of userNumber

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -1,1 +1,14 @@
-alert('ciao');
+let userNumber = parseInt(prompt('Inserisci un numero'));
+let result;
+
+while (isNaN(userNumber)) {
+    alert('Non hai inserito un numero!');
+    userNumber = parseInt(prompt('Inserisci un numero'));
+}
+
+console.log(userNumber, typeof(userNumber));
+
+for (let i = userNumber; i > 0; i--) {
+    result = `${i} - ${i*i*i}`;
+    console.log(result);
+}
